e90e635fdd756d1682e2e146f5311151ea8fb2ec
Thomas Grundström <thomas.grundstrom@helsinki.fi>
2023-12-06 17:51:30 +0200

    tehtava 5

--- viikko6/query-language/src/queries.py
+++ viikko6/query-language/src/queries.py
@@ -6,6 +6,16 @@ class Matcher:
     
     def test(self, player):
         return self._matcher.test(player)
+
+class OrMatcher:
+    def __init__(self, choices):
+        self._choices = choices
+    
+    def test(self, player):
+        for i in range(len(self._choices)-1):
+            if Or(self._choices[i], self._choices[i+1]).test(player):
+                return True
+        return False
         
 
 class QueryBuilder:
@@ -23,3 +33,6 @@ class QueryBuilder:
     
     def hasFewerThan(self, value, attr):
         return QueryBuilder(Matcher(self._matcher, HasFewerThan(value, attr)))
+
+    def oneOf(self, *choices):
+        return QueryBuilder(OrMatcher(choices))
